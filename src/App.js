d46a38aa259d83133ae0f72df3ab21d1f0814dee
VisarutDear <61011358@kmitl.ac.th>
2022-01-20 16:40:18 +0700

    add voice feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import MicRecorder from 'mic-recorder-to-mp3';
+import {useState, useEffect, useMemo} from 'react';
 
 function App() {
+  const recorder = useMemo(() => new MicRecorder({bitRate: 128}), []);
+  const [micState, setMicState] = useState({
+    isRecording: false,
+    blobURL: '',
+    isBlocked: false,
+  });
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia(
+      {audio: true},
+      () => {
+        console.log('Permission Granted');
+        setMicState({...micState, isBlocked: false});
+      },
+      () => {
+        console.log('Permission Denied');
+        setMicState({...micState, isBlocked: true});
+      }
+    );
+  }, []);
+
+  const start = () => {
+    if (micState.isBlocked) {
+      console.log('Permission Denied');
+    } else {
+      console.log('Recording');
+      recorder
+        .start()
+        .then(() => {
+          setMicState({...micState.isRecording, isRecording: true});
+        })
+        .catch(e => console.log(e));
+    }
+  };
+
+  const stop = () => {
+    recorder
+      .stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        // const blobURL = URL.createObjectURL(blob);
+        console.log(buffer, blob);
+        const file = new File(buffer, 'music.mp3', {
+          type: blob.type,
+          lastModified: Date.now(),
+        });
+        const li = document.createElement('li');
+        const player = new Audio(URL.createObjectURL(file));
+        player.controls = true;
+        li.appendChild(player);
+        document.querySelector('#playlist').appendChild(li);
+        setMicState({...micState, isRecording: false});
+      })
+      .catch(e => console.log(e));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>dear</h1>
+      <button onClick={start} disabled={micState.isRecording}>
+        Record
+      </button>
+      <button onClick={stop} disabled={!micState.isRecording}>
+        Stop
+      </button>
+      {/* <audio src={micState.blobURL} controls="controls" /> */}
+      <ul id="playlist"></ul>
+    </>
   );
 }
 
