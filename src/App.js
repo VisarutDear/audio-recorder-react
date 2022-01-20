c40643c8b82c6eb95c8d26241aff2c96685f443b
VisarutDear <61011358@kmitl.ac.th>
2022-01-20 17:10:45 +0700

    comment code

--- src/App.js
+++ src/App.js
@@ -2,73 +2,72 @@ import MicRecorder from 'mic-recorder-to-mp3';
 import {useState, useEffect, useMemo} from 'react';
 
 function App() {
-  const recorder = useMemo(() => new MicRecorder({bitRate: 128}), []);
-  const [micState, setMicState] = useState({
-    isRecording: false,
-    blobURL: '',
-    isBlocked: false,
-  });
+  // const recorder = useMemo(() => new MicRecorder({bitRate: 128}), []);
+  // const [micState, setMicState] = useState({
+  //   isRecording: false,
+  //   blobURL: '',
+  //   isBlocked: false,
+  // });
 
-  useEffect(() => {
-    navigator.mediaDevices.getUserMedia(
-      {audio: true},
-      () => {
-        console.log('Permission Granted');
-        setMicState({...micState, isBlocked: false});
-      },
-      () => {
-        console.log('Permission Denied');
-        setMicState({...micState, isBlocked: true});
-      }
-    );
-  }, []);
+  // useEffect(() => {
+  //   navigator.mediaDevices.getUserMedia(
+  //     {audio: true},
+  //     () => {
+  //       console.log('Permission Granted');
+  //       setMicState({...micState, isBlocked: false});
+  //     },
+  //     () => {
+  //       console.log('Permission Denied');
+  //       setMicState({...micState, isBlocked: true});
+  //     }
+  //   );
+  // }, []);
 
-  const start = () => {
-    if (micState.isBlocked) {
-      console.log('Permission Denied');
-    } else {
-      console.log('Recording');
-      recorder
-        .start()
-        .then(() => {
-          setMicState({...micState.isRecording, isRecording: true});
-        })
-        .catch(e => console.log(e));
-    }
-  };
+  // const start = () => {
+  //   if (micState.isBlocked) {
+  //     console.log('Permission Denied');
+  //   } else {
+  //     console.log('Recording');
+  //     recorder
+  //       .start()
+  //       .then(() => {
+  //         setMicState({...micState.isRecording, isRecording: true});
+  //       })
+  //       .catch(e => console.log(e));
+  //   }
+  // };
 
-  const stop = () => {
-    recorder
-      .stop()
-      .getMp3()
-      .then(([buffer, blob]) => {
-        // const blobURL = URL.createObjectURL(blob);
-        console.log(buffer, blob);
-        const file = new File(buffer, 'music.mp3', {
-          type: blob.type,
-          lastModified: Date.now(),
-        });
-        const li = document.createElement('li');
-        const player = new Audio(URL.createObjectURL(file));
-        player.controls = true;
-        li.appendChild(player);
-        document.querySelector('#playlist').appendChild(li);
-        setMicState({...micState, isRecording: false});
-      })
-      .catch(e => console.log(e));
-  };
+  // const stop = () => {
+  //   recorder
+  //     .stop()
+  //     .getMp3()
+  //     .then(([buffer, blob]) => {
+  //       // const blobURL = URL.createObjectURL(blob);
+  //       console.log(buffer, blob);
+  //       const file = new File(buffer, 'music.mp3', {
+  //         type: blob.type,
+  //         lastModified: Date.now(),
+  //       });
+  //       const li = document.createElement('li');
+  //       const player = new Audio(URL.createObjectURL(file));
+  //       player.controls = true;
+  //       li.appendChild(player);
+  //       document.querySelector('#playlist').appendChild(li);
+  //       setMicState({...micState, isRecording: false});
+  //     })
+  //     .catch(e => console.log(e));
+  // };
 
   return (
     <>
       <h1>dear</h1>
-      <button onClick={start} disabled={micState.isRecording}>
+      {/* <button onClick={start} disabled={micState.isRecording}>
         Record
       </button>
       <button onClick={stop} disabled={!micState.isRecording}>
         Stop
       </button>
-      {/* <audio src={micState.blobURL} controls="controls" /> */}
-      <ul id="playlist"></ul>
+      <ul id="playlist"></ul> */}
     </>
   );
 }
